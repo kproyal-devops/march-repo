9e7ad29b4f489589a434a706cba208719b6211bb
kproyal-devops <purushothamkunapalli@gmail.com>
2026-03-31 11:52:57 +0530

    added some code

--- app.py
+++ app.py
@@ -8,3 +8,12 @@ num = 8
 
 num_sqrt = num ** 0.5
 print('The square root of %0.3f is %0.3f'%(num ,num_sqrt))
+
+
+num = float(input("Enter a number: "))
+if num > 0:
+   print("Positive number")
+elif num == 0:
+   print("Zero")
+else:
+   print("Negative number")
